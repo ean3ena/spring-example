6cc20ac2174d063114215319ee9c044a92bc2706
ean3ena <ean3ena@yandex.ru>
2025-01-09 15:53:47 +0300

    init commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "io.hexlet"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
